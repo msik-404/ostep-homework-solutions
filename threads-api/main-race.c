39228786010a5341c3da371664bbb4eb84713300
mateusz <siiikor.sikorski@gmail.com>
2024-01-19 16:19:38 +0100

    Threads-api homework is done.

--- threads-api/main-race.c
+++ threads-api/main-race.c
@@ -4,15 +4,33 @@
 
 int balance = 0;
 
-void* worker(void* arg) {
-    balance++; // unprotected access 
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+void* worker(void* arg)
+{
+    int rc = pthread_mutex_lock(&lock);
+    assert(rc == 0);
+
+    balance++; // unprotected access
+
+    rc = pthread_mutex_unlock(&lock);
+    assert(rc == 0);
+
     return NULL;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[])
+{
     pthread_t p;
     Pthread_create(&p, NULL, worker, NULL);
+    int rc = pthread_mutex_lock(&lock);
+    assert(rc == 0);
+
     balance++; // unprotected access
+
+    rc = pthread_mutex_unlock(&lock);
+    assert(rc == 0);
+
     Pthread_join(p, NULL);
     return 0;
 }
